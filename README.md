5e22053cac4365851f48ed9cc6ef2dd15506793c
Makai <danielmakai@gmail.com>
2024-08-06 09:10:31 +0300

    AirBnB

---
+++ README.md
@@ -0,0 +1,39 @@
+# AirBnB Clone - The Console
+The console is the first segment of the AirBnB project at Holberton School that will collectively cover fundamental concepts of higher level programming. The goal of AirBnB project is to eventually deploy our server a simple copy of the AirBnB Website(HBnB). A command interpreter is created in this segment to manage objects for the AirBnB(HBnB) website.
+
+#### Functionalities of this command interpreter:
+* Create a new object (ex: a new User or a new Place)
+* Retrieve an object from a file, a database etc...
+* Do operations on objects (count, compute stats, etc...)
+* Update attributes of an object
+* Destroy an object
+
+## Resources
+Read or watch:
+* REST API concept page
+* Learn REST: A RESTful Tutorial
+* Designing a RESTful API with Python and Flask
+* HTTP access control (CORS)
+* Flask cheatsheet
+* What are Flask Blueprints, exactly?
+* Flask
+* Modular Applications with Blueprints
+* Flask tests
+* Flask-CORS
+
+## Learning Objectives
+At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
+
+## Requirements
+### Python Scripts
+* Allowed editors: vi, vim, emacs
+* All your files will be interpreted/compiled on Ubuntu 20.04 LTS using python3 (version 3.4.3)
+* The first line of all your files should be exactly #!/usr/bin/python3
+* our code should use the PEP 8 style (version 1.7)
+* All your modules should have documentation (python3 -c
+
+## Installation
+* Clone this repository: `git clone "https://github.com/alexaorrico/AirBnB_clone.git"`
+* Access AirBnb directory: `cd AirBnB_clone`
+* Run hbnb(interactively): `./console` and enter command
+* Run hbnb(non-interactively): `echo "<command>" | ./console.py` 
---
+++ api/v1/views/states.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""
+a new view for State objects that handles all default RESTFul API actions:
+"""
+
+from flask import abort, jsonify, make_response, request
+from api.v1.views import app_views
+from models import storage
+from models.state import State
+
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def state():
+    '''Retrieves the list of all State objects'''
+    objs = storage.all(State)
+    return jsonify([obj.to_dict() for obj in objs.values()])
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def single_state(state_id):
+    '''Retrieves a State object'''
+    obj = storage.get(State, state_id)
+    if not obj:
+        abort(404)
+    return jsonify(obj.to_dict())
+
+
+@app_views.route('/states/<state_id>',
+                 methods=['DELETE'], strict_slashes=False)
+def del_state(state_id):
+    '''Deletes a State object'''
+    obj = storage.get(State, state_id)
+    if not obj:
+        abort(404)
+    obj.delete()
+    storage.save()
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def post_state():
+    """Returns the new State with the status code 201"""
+    new_obj = request.get_json()
+    if not new_obj:
+        abort(400, "Not a JSON")
+    if 'name' not in new_obj:
+        abort(400, "Missing name")
+    obj = State(**new_obj)
+    storage.new(obj)
+    storage.save()
+    return make_response(jsonify(obj.to_dict()), 201)
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def put_state(state_id):
+    """ Updates a State object """
+    obj = storage.get(State, state_id)
+    if not obj:
+        abort(404)
+
+    req = request.get_json()
+    if not req:
+        abort(400, "Not a JSON")
+
+    for key, value in req.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(obj, key, value)
+
+    storage.save()
+    return make_response(jsonify(obj.to_dict()), 200)
